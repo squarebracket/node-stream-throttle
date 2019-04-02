e4a0bf976e83bc1989afc96f98a2f35d95cf7d93
Chuck Wilson <chuck.wilson+github@gmail.com>
2019-04-02 16:22:43 -0400

    working

--- src/throttle.js
+++ src/throttle.js
@@ -12,9 +12,18 @@ var TokenBucket = require('limiter').TokenBucket;
 function Throttle(opts, group) {
     if (group === undefined)
         group = new ThrottleGroup(opts);
+    this.group = group;
     this.bucket = group.bucket;
     this.chunksize = group.chunksize;
+    //this.bucket = new TokenBucket(opts.rate, opts.rate, 'second', null);
+    //this.chunksize = opts.rate / 10;
     Transform.call(this, opts);
+    this.on('end', () => {
+      let asdf = this.group.throttles.indexOf(this);
+      if (asdf > -1) {
+        delete this.group.throttles[asdf];
+      }
+    });
 }
 inherits(Throttle, Transform);
 
@@ -22,20 +31,51 @@ Throttle.prototype._transform = function(chunk, encoding, done) {
     process(this, chunk, 0, done);
 };
 
+Throttle.prototype.setRate = function(rate) {
+    //this._pendingRate = rate;
+    this.group.setRate(rate);
+};
+
 function process(self, chunk, pos, done) {
-    var slice = chunk.slice(pos, pos + self.chunksize);
+    var slice = chunk.slice(pos, pos + 1024);
+    //var slice = chunk.slice(pos, pos + self.chunksize);
     if (!slice.length) {
         // chunk fully consumed
         done();
         return;
     }
-    self.bucket.removeTokens(slice.length, function(err) {
+    if (self.group.changingRates) {
+      console.log('waiting until rate change is over');
+      setTimeout(() => {
+        process(self, chunk, pos, done);
+      });
+      return;
+    }
+    //const chunksize = self.chunksize;
+    const chunksize = 1024;
+    //console.log(slice.length, chunksize, self.chunksize);
+    var worked = self.bucket.removeTokens(slice.length, function(err) {
         if (err) {
+            console.error('error', err);
             done(err);
             return;
         }
         self.push(slice);
-        process(self, chunk, pos + self.chunksize, done);
+        var end = pos + chunksize;
+        //if (self._pendingRate) {
+          //console.log(`changing rate from ${self.bucket.tokensPerInterval} to ${self._pendingRate}`);
+          //self.bucket.tokensPerInterval = self._pendingRate;
+          //self.chunksize = self._pendingRate / 10;
+          //self._pendingRate = null;
+        //}
+        //if (self.group._pendingRate) {
+          //console.log(`changing rate from ${self.bucket.tokensPerInterval} to ${self.group._pendingRate}`);
+          //self.bucket.tokensPerInterval = self.group._pendingRate;
+          //self.bucket.bucketSize = self.group._pendingRate;
+          //self.chunksize = self.group._pendingRate / 10;
+          //self.group._pendingRate = null;
+        //}
+        process(self, chunk, end, done);
     });
 }
 
@@ -56,9 +96,11 @@ function ThrottleGroup(opts) {
         throw new Error('throttle chunk size must be a positive number');
     }
 
+    this.throttles = [];
     this.rate = opts.rate;
-    this.chunksize = opts.chunksize || this.rate/10;
-    this.bucket = new TokenBucket(this.rate, this.rate, 'second', null);
+    this.originalChunksize = opts.chunksize;
+    this.chunksize = this.originalChunksize || this.rate/20;
+    this.bucket = new TokenBucket(this.rate / 20, this.rate, 1000, null);
 }
 
 /*
@@ -66,7 +108,20 @@ function ThrottleGroup(opts) {
  * Any supplied options are passed to the Throttle constructor.
  */
 ThrottleGroup.prototype.throttle = function(opts) {
-    return new Throttle(opts, this);
+    const throttle = new Throttle(opts, this);
+    this.throttles.push(throttle);
+    return throttle;
+};
+
+ThrottleGroup.prototype.setRate = function(rate) {
+    this.changingRates = true;
+    this.bucket.tokensPerInterval = rate;
+    this.bucket.bucketSize = rate / 20;
+    this.chunksize = this.originalChunksize || (rate / 20);
+    this.throttles.forEach((throttle) => {
+      throttle.chunksize = this.originalChunksize || (rate / 20);
+    });
+    this.changingRates = false;
 };
 
 module.exports = {
